e61d8f53e650f8cded986510af3d13c5f83a0493
Alejiri <faramirezs@gmail.com>
2026-05-07 12:25:07 +0200

    301 moved to 308 and vite env removed

--- frontend/kitchen/src/vite-env.d.ts
+++ frontend/kitchen/src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_AGENT_URL: string;
   readonly VITE_OFFICE_URL?: string;
   readonly VITE_AGENT_DEBUG_STREAM?: string;
 }
